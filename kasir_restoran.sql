b82e8c088706b5e602280ea265a230e6df35dd12
fabrianivan <fabrianivan@users.noreply.github.com>
2016-12-09 07:55:25 +0700

    Add files via upload

--- kasir_restoran.sql
+++ kasir_restoran.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 08, 2016 at 01:34 PM
+-- Generation Time: Dec 09, 2016 at 01:54 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -34,6 +34,14 @@ CREATE TABLE `history` (
   `harga` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`no_pembelian`, `barang`, `jumlah`, `satuan`, `harga`) VALUES
+(1, 'Spicy', 1, 28000, 28000),
+(2, 'Spicy', 1, 28000, 28000);
+
 -- --------------------------------------------------------
 
 --
@@ -52,8 +60,13 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`code`, `name`, `price`, `stock`) VALUES
-('1', 'Nasi Goreng', 19000, 300),
-('2', 'Ayam Bakar', 19000, 330);
+('1', 'Spicy', 28000, 298),
+('2', 'SteakCHeese', 35000, 328),
+('3', 'SubwayClub', 40000, 329),
+('4', 'SubwayClub Avocado', 28000, 329),
+('5', 'Subway Melt', 33000, 329),
+('6', 'JOHN COLLIAS', 21000, 299),
+('7', 'LONG ISLAND ICED TEA', 15000, 299);
 
 -- --------------------------------------------------------
 
@@ -70,14 +83,6 @@ CREATE TABLE `order_menu` (
   `total` int(11) NOT NULL,
   `completed` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `order_menu`
---
-
-INSERT INTO `order_menu` (`id`, `id_user`, `menu`, `price`, `quantity`, `total`, `completed`) VALUES
-(1, 1, 'Nasi Goreng', 19000, 5, 95000, 0),
-(2, 1, 'Ayam Bakar', 19000, 5, 95000, 0);
 
 -- --------------------------------------------------------
 
@@ -136,6 +141,20 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `fullname` (`fullname`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `no_pembelian` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `order_menu`
+--
+ALTER TABLE `order_menu`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
